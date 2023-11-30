3f11469c13f1f33f1da2b0451246df4b2e7dc666
Marlon.Developer <150313937+Marlon-WebDeveloper@users.noreply.github.com>
2023-11-30 01:13:13 -0500

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector(".navbar-toggler");
+const lista  = document.querySelector(".list");
+const close = document.querySelector(".btn-close-dark")
+
+
+btn.addEventListener("click",()=>{
+btn.style.display = `none`
+lista.style.visibility = `visible`
+close.style.visibility = `visible`
+
+lista.style.transform = `translateX(0)`
+close.style.transform = `translateX(0)`
+})
+close.addEventListener("click",()=>{
+    lista.style.transform = `translateX(100%)`
+    close.style.transform = `translateX(100%)`
+    btn.style.display = `block`
+    lista.style.visibility = `hidden`
+close.style.visibility = `hidden`
+})
